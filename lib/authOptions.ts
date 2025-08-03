590e6f208c28ca613faec53a446e7893d3468504
hsibtain18 <33388697+hsibtain18@users.noreply.github.com>
2025-08-04 03:15:27 +0400

    temp

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -15,7 +15,6 @@ const userPool = new CognitoUserPool({
   UserPoolId: process.env.COGNITO_USER_POOL_ID!,
   ClientId: process.env.COGNITO_CLIENT_ID!,
 });
-
 export const authOptions = {
   providers: [
     GoogleProvider({
